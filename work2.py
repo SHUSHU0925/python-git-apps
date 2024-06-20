c58cfbef31c6d3a6af8f249d04d94f21e21a2704
SHUSHU0925 <emo11280925@gmail.com>
2024-06-20 11:34:25 +0900

    OW

--- work2.py
+++ work2.py
@@ -13,40 +13,26 @@ window.configure(bg=bg_color)
 def button_action1():  # 関数の定義 ※ボタンが押されたときの動き
     seireki = int(num.get())
     if seireki < 1912:
-        wareki = "明治"
+        gengo = "明治"
         wareki = seireki - 1868 + 1
-        if wareki == 1:
-            label1.config(text=f"西暦{seireki}年は明治元年です")
-        else:
-            label1.config(text=f"西暦{seireki}年は明治{wareki}年です")
     elif seireki < 1926:
-        wareki = "大正"
+        gengo = "大正"
         wareki = seireki - 1912 + 1
-        if wareki == 1:
-            label1.config(text=f"西暦{seireki}年は大正元年です")
-        else:
-            label1.config(text=f"西暦{seireki}年は大正{wareki}年です")
     elif seireki < 1989:
-        wareki = "昭和"
+        gengo = "昭和"
         wareki = seireki - 1926 + 1
-        if wareki == 1:
-            label1.config(text=f"西暦{seireki}年は昭和元年です")
-        else:
-            label1.config(text=f"西暦{seireki}年は昭和{wareki}年です")
     elif seireki < 2019:
-        wareki = "平成"
+        gengo = "平成"
         wareki = seireki - 1989 + 1
-        if wareki == 1:
-            label1.config(text=f"西暦{seireki}年は平成元年です")
-        else:
-            label1.config(text=f"西暦{seireki}年は平成{wareki}年です")
-    elif seireki > 2019:
-        wareki = "令和"
+    else:
+        seireki > 2019
+        gengo = "令和"
         wareki = seireki - 2019 + 1
-        if wareki == 1:
-            label1.config(text=f"西暦{seireki}年は令和元年です")
-        else:
-            label1.config(text=f"西暦{seireki}年は令和{wareki}年です")
+
+    if wareki == 1:
+        label1.config(text=f"西暦{seireki}年は{gengo}元年です")
+    else:
+        label1.config(text=f"西暦{seireki}年は{gengo}{wareki}年です")
 
 
 # 入力フィールドの作成
